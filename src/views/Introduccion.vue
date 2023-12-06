b2dccb8a7f817b55ac35fb3b64f48c49b882124d
Luis Pérez <ljpm9401@gmail.com>
2023-12-06 08:38:46 -0500

    Ajuste video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,7 +12,7 @@
       p(data-aos="fade-right") Le damos la bienvenida al estudio de este componente. Para comenzar, revise el siguiente video, que aquí se muestra. #[strong ¡Adelante!]
       figure.mt-4(data-aos="zoom-in" style="position:relative;z-index:2;")
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/pm9bgkdqRBg" title="Mejoramiento continuo de las estrategias de ciberseguridad" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     .cont_intro2.pb-4
       p.mt-5( data-aos="zoom-in-up") Para la elaboración de este componente, se abordaron varios autores conocidos en #[strong mejoramiento continuo de las estrategias de ciberseguridad], de quienes se han citado y referenciado conceptos y ejemplos para los fines educativos de este curso, en el entendido de que el conocimiento es social y, por lo tanto, es para ser usado por quienes necesitan adquirirlo. Se espera que este documento sea útil para todos aquellos, aprendices y lectores en general, que estén interesados en acercarse a asuntos básicos de la #[strong implementación y operación de la ciberseguridad].
 
